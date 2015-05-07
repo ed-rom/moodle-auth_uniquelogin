aa2ddb395fc69f7009dc437f7fdd9b803a9f9c69
ed-rom <asfS2$SFA!>
2015-05-07 13:09:40 +0100

    Send Files

---
+++ lang/en/auth_uniquelogin.php
@@ -0,0 +1,10 @@
+<?php
+
+$string['pluginname'] = 'Unique Login';
+$string['auth_uniquelogintitle'] = 'Unique login';
+$string['auth_uniquelogindescription'] = 'This login ensures thar each user only have one active session.<br /><br />
+Every time a user makes a sucessfull login, all other session belonging to that user will be terminated.<br><br />
+<div style=\"font-weight: bold;\">Note 1: For this plugin to work, the user sessions mus be stored in database. This configuration is set in <a href="settings.php?section=sessionhandling">Sessions.</a></div><br />
+';
+
+?>
---
+++ lang/pt/auth_uniquelogin.php
@@ -0,0 +1,9 @@
+<?php
+
+$string['pluginname'] = 'Login Único';
+$string['auth_uniquelogintitle'] = 'Login único';
+$string['auth_uniquelogindescription'] = 'Este plugin faz com que cada utilizador apenas possa ter uma sessão activa simultaneamente.<br /><br />
+Sempre que um utilizador fizer login com sucesso, todas as sessões desse utilizador que estejam activas são automaticamente terminadas.<br><br />
+<div style=\"font-weight: bold;\">Nota 1: Este plugin implica que as sessões dos utilizadores sejam guardadas em base de dados. Essa configuração pode ser feita na secção Gestão de sessões.</div><br />';
+
+?>
